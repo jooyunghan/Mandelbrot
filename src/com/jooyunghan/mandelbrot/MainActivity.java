9c2165c83bd025fd6eb2933b799969264fe22d3c
Jooyung Han <jooyung.han@gmail.com>
2012-10-15 05:24:01 -0700

    Revert "add asynctask to draw mandelbrot patch"
    This reverts commit 39040e0ee046248a69d81ca73ca9841ed9fab917.

--- src/com/jooyunghan/mandelbrot/MainActivity.java
+++ src/com/jooyunghan/mandelbrot/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MandelbrotView2(this));
+        setContentView(new MandelbrotView1(this));
     }
 
     @Override
@@ -17,6 +17,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
-    
 }
